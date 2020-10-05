dcf6060ab0150dbf6e13d54783b39e8ab6618e31
unknown <plocieniak@interia.pl>
2020-10-05 18:36:52 +0200

    Updates

---
+++ precache-manifest.3552d8bd294a5fe850ea9b27a655041b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e8953d4ee9c2ca5f44d0c0c71e3d0f",
+    "url": "/voltic/index.html"
+  },
+  {
+    "revision": "82a26108ed6e071cd3f2",
+    "url": "/voltic/static/css/main.e4e40702.chunk.css"
+  },
+  {
+    "revision": "68956d2ab27879dd5942",
+    "url": "/voltic/static/js/2.c317853a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/voltic/static/js/2.c317853a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a26108ed6e071cd3f2",
+    "url": "/voltic/static/js/main.40c22e45.chunk.js"
+  },
+  {
+    "revision": "b76eff06ab9385dde0c4",
+    "url": "/voltic/static/js/runtime-main.32f5e762.js"
+  },
+  {
+    "revision": "c650bc8c2c9a40c006a691cbbcd45bf2",
+    "url": "/voltic/static/media/logo.c650bc8c.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/voltic/precache-manifest.3552d8bd294a5fe850ea9b27a655041b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/voltic/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
